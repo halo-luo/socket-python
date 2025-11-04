0bdef790c2c5d1783245d35a1e198ff99b85c867
llf <bingo.llf.00@gmail.com>
2025-11-04 15:22:49 +0800

    upload signle thread socket code

--- src/client.py
+++ src/client.py
@@ -1,6 +1,24 @@
 import socket
 
+def client():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('127.0.0.1', 65501))
+    while True:    
+        ipt = input()
+        if len(ipt) > 0:
+            
+            send_data = ipt.encode('gbk')
+            client_socket.send(send_data)
+            
+            if ipt == "q":
+                break
+
+            recv_data = client_socket.recv(1024)
+            recv_content = recv_data.decode('gbk')
+            print("客户端接收数据为：", recv_content)
+    client_socket.close()
+
 
 if __name__ == "__main__":
     print("client start...")
-    
+    client()
--- src/server.py
+++ src/server.py
@@ -1,6 +1,28 @@
 import socket
 
+def server():
+    # 创建socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+    server_socket.bind(("", 65501))
+    server_socket.listen(1)
+    server_client_socket, client_prot = server_socket.accept()
+    while True:
+        
+        recv_data = server_client_socket.recv(1024)
+        recv_content = recv_data.decode("gbk")
+        if len(recv_content) > 0:
+            print(recv_content)
+            if recv_content == 'q':
+                break
+            recv_content = "已收到：" + recv_content
+            recv_content = recv_content.encode('gbk')
+        server_client_socket.send(recv_content)
+    
+    server_client_socket.close()
+    server_socket.close()
+
 
 if __name__ == "__main__":
     print("server start...")
-    
+    server()
